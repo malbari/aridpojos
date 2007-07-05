9fca5c1166c433376f80b4cada4341b4e7722b5f
chris.e.richardson <chris.e.richardson@c626f77e-ce2e-0410-b5f4-5bc66e99e280>
2007-07-05 17:31:25 +0000

    Wrote some more tests and fixed bugs

---
+++ arid-dao/src/test/java/net/chrisrichardson/arid/dao/hibernate/CriteriaQueryFactoryTests.java
@@ -0,0 +1,89 @@
+package net.chrisrichardson.arid.dao.hibernate;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.jmock.Expectations;
+import org.jmock.integration.junit3.MockObjectTestCase;
+import org.jmock.lib.legacy.ClassImposteriser;
+
+public class CriteriaQueryFactoryTests extends MockObjectTestCase {
+
+	private CriteriaBuilder criteriaBuilder;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		setImposteriser(ClassImposteriser.INSTANCE);
+		criteriaBuilder = mock(CriteriaBuilder.class); 
+	}
+	
+	class MyCriteriaQueryFactory extends CriteriaQueryFactory {
+
+		public MyCriteriaQueryFactory(Class<?> entityClass, String methodName) {
+			super(entityClass, methodName);
+		}
+		
+		@Override
+		protected CriteriaBuilder makeCriteriaBuilder(Object[] args) {
+			super.makeCriteriaBuilder(args);
+			return criteriaBuilder;
+		}
+		
+	}
+	
+	public void testFindAll() {
+		CriteriaQueryFactory factory = new MyCriteriaQueryFactory(Foo.class, "findAll");
+		checking(new Expectations() {{
+			one(criteriaBuilder).getCriteriaQuery();
+		}}
+		);
+		DetachedCriteria result = factory.makeCriteriaQuery(new Object[]{"x", 1, 5});
+		assertNotNull(result);
+	}
+
+	public void testMultiple() {
+		CriteriaQueryFactory factory = new MyCriteriaQueryFactory(Foo.class, "findByBarEqualBazBetween");
+		checking(new Expectations() {{
+			one(criteriaBuilder).processKeyword("Equal", "bar");
+			one(criteriaBuilder).processKeyword("Between", "baz");
+			one(criteriaBuilder).getCriteriaQuery();
+		}}
+		);
+		DetachedCriteria result = factory.makeCriteriaQuery(new Object[]{"x", 1, 5});
+		assertNotNull(result);
+	}
+
+	public void testFindByJustProperty() {
+		CriteriaQueryFactory factory = new MyCriteriaQueryFactory(Foo.class, "findByBar");
+		checking(new Expectations() {{
+			one(criteriaBuilder).handleEq("bar");
+			one(criteriaBuilder).getCriteriaQuery();
+		}}
+		);
+		DetachedCriteria result = factory.makeCriteriaQuery(new Object[]{"x"});
+		assertNotNull(result);
+	}
+
+	public void testFindRequiredByJustProperty() {
+		CriteriaQueryFactory factory = new MyCriteriaQueryFactory(Foo.class, "findRequiredByBar");
+		checking(new Expectations() {{
+			one(criteriaBuilder).handleEq("bar");
+			one(criteriaBuilder).getCriteriaQuery();
+		}}
+		);
+		DetachedCriteria result = factory.makeCriteriaQuery(new Object[]{"x"});
+		assertNotNull(result);
+	}
+
+	public void testTwoThings() {
+		CriteriaQueryFactory factory = new MyCriteriaQueryFactory(Foo.class, "findByBarEqualBaz");
+		checking(new Expectations() {{
+			one(criteriaBuilder).processKeyword("Equal", "bar");
+			one(criteriaBuilder).handleEq("baz");
+			one(criteriaBuilder).getCriteriaQuery();
+		}}
+		);
+		DetachedCriteria result = factory.makeCriteriaQuery(new Object[]{"x", 1, 5});
+		assertNotNull(result);
+	}
+
+}
---
+++ arid-dao/src/test/java/net/chrisrichardson/arid/dao/hibernate/Foo.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.chrisrichardson.arid.dao.hibernate;
+
+class Foo {
+	private String bar;
+	private int baz;
+	
+	public String getBar() {
+		return bar;
+	}
+	public int getBaz() {
+		return baz;
+	}
+}

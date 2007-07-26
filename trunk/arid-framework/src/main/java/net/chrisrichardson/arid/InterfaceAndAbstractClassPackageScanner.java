e9a135a4e25ec346e516ad90ec9d0c42a6aaa3c5
chris.e.richardson <chris.e.richardson@c626f77e-ce2e-0410-b5f4-5bc66e99e280>
2007-07-26 04:28:13 +0000

    cleaned up derived type calculation and wrote tests

--- trunk/arid-framework/src/main/java/net/chrisrichardson/arid/InterfaceAndAbstractClassPackageScanner.java
+++ trunk/arid-framework/src/main/java/net/chrisrichardson/arid/InterfaceAndAbstractClassPackageScanner.java
@@ -1,7 +1,5 @@
 package net.chrisrichardson.arid;
 
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Scans for abstract classes and interfaces and returns the most derived types
@@ -12,26 +10,6 @@ public class InterfaceAndAbstractClassPackageScanner extends
 	@Override
 	protected boolean isMatch(Class<?> type) {
 		return !isConcreteClass(type);
-	}
-
-	@Override
-	protected List<Class> addNewType(List<Class> result, Class type) {
-		if (removeSupertypes(result, type))
-			return super.addNewType(result, type);
-		else
-			return result;
-	}
-
-	private boolean removeSupertypes(List<Class> result, Class type) {
-		for (Iterator<Class> it = result.iterator(); it.hasNext();) {
-			Class<?> existingClass = it.next();
-			if (existingClass.isAssignableFrom(type)) {
-				it.remove();
-			} else if (type.isAssignableFrom(existingClass)) {
-				return false;
-			}
-		}
-		return true;
 	}
 
 }
